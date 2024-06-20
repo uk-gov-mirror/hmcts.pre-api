b8a937bc028c9efddf973ff3e849a3dfabbf4046
Marianne Azzopardi <76755256+marazzo@users.noreply.github.com>
2024-06-20 14:47:51 +0100

    change to flyway migration to update only cases not deleted with the OPEN state

--- src/main/resources/db/migration/V020__AddStateFieldForCases.sql
+++ src/main/resources/db/migration/V020__AddStateFieldForCases.sql
@@ -7,12 +7,18 @@ CREATE TYPE public.CASE_STATE AS ENUM (
 
 -- Alter cases table to include state and closed_at columns
 ALTER TABLE public.cases
-    ADD COLUMN state CASE_STATE NOT NULL DEFAULT 'OPEN',
+    ADD COLUMN state CASE_STATE NOT NULL,
     ADD COLUMN closed_at TIMESTAMPTZ;
+
+-- Update existing cases to default state OPEN where deleted_at is NULL
+UPDATE public.cases 
+SET state = 'OPEN' 
+WHERE deleted_at IS NULL;
+
+-- Set the default value of state to OPEN for future inserts
+ALTER TABLE public.cases ALTER COLUMN state SET DEFAULT 'OPEN';
 
 -- Constraint to ensure closed_at is not in the future
 ALTER TABLE public.cases
     ADD CONSTRAINT check_closed_at_not_in_future CHECK (closed_at <= NOW());
 
--- Update existing cases to default state OPEN
-UPDATE public.cases SET state = 'OPEN'; 
