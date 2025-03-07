a60285a934387aacbad09e38342e3704a76cf22b
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-03-07 15:31:42 +0000

    move x-user-id to to centralised place

--- src/main/java/uk/gov/hmcts/reform/preapi/config/OpenAPIConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/OpenAPIConfiguration.java
@@ -1,22 +1,14 @@
 package uk.gov.hmcts.reform.preapi.config;
 
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.UUIDSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OperationCustomizer;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerMethod;
-
 
 @Configuration
 public class OpenAPIConfiguration {
@@ -28,45 +20,22 @@ public class OpenAPIConfiguration {
     public OpenAPI openAPI() {
         return new OpenAPI()
             .info(new Info().title("PRE API")
-                            .description("PRE API - Used for managing courts, bookings, recordings and permissions.")
-                            .version("v0.0.1")
-                            .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
-            .addSecurityItem(new SecurityRequirement().addList(APIM_SUBSCRIPTION_KEY_HEADER))
+                      .description("PRE API - Used for managing courts, bookings, recordings and permissions.")
+                      .version("v0.0.1")
+                      .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
+            .addSecurityItem(new SecurityRequirement().addList(APIM_SUBSCRIPTION_KEY_HEADER).addList(X_USER_ID_HEADER))
             .components(
                 new Components()
                     .addSecuritySchemes(APIM_SUBSCRIPTION_KEY_HEADER,
                                         new SecurityScheme()
                                             .type(SecurityScheme.Type.APIKEY)
                                             .in(SecurityScheme.In.HEADER)
-                                            .name(APIM_SUBSCRIPTION_KEY_HEADER)
-                    )
-            )
-            .externalDocs(new ExternalDocumentation()
-                              .description("README")
-                              .url("https://github.com/hmcts/pre-api"));
+                                            .name(APIM_SUBSCRIPTION_KEY_HEADER))
+                    .addSecuritySchemes(X_USER_ID_HEADER,
+                                        new SecurityScheme()
+                                            .type(SecurityScheme.Type.APIKEY)
+                                            .in(SecurityScheme.In.HEADER)
+                                            .name(X_USER_ID_HEADER)))
+            .externalDocs(new ExternalDocumentation().description("README").url("https://github.com/hmcts/pre-api"));
     }
-
-    @Bean
-    public GroupedOpenApi publicApi(OperationCustomizer customGlobalHeaders) {
-        return GroupedOpenApi.builder()
-            .group("pre-api")
-            .addOperationCustomizer(customGlobalHeaders)
-            .build();
-    }
-
-    @Bean
-    public OperationCustomizer customGlobalHeaders() {
-        return (Operation customOperation, HandlerMethod handlerMethod) -> {
-            Parameter serviceAuthorizationHeader = new Parameter()
-                .in(ParameterIn.HEADER.toString())
-                .schema(new UUIDSchema())
-                .name(X_USER_ID_HEADER)
-                .description("The User Id of the User making the request")
-                .example("123e4567-e89b-12d3-a456-426614174000")
-                .required(false); // set to true once Power Platform is updated.
-            customOperation.addParametersItem(serviceAuthorizationHeader);
-            return customOperation;
-        };
-    }
-
 }
